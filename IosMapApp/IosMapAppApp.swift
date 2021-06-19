277563e750159e45a1370163d386eafc0db8c5ce
Nesim Alma <48694570+Nesim11@users.noreply.github.com>
2021-06-19 21:01:18 +0300

    Initial commit

---
+++ IosMapApp/IosMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosMapAppApp.swift
+//  IosMapApp
+//
+//  Created by Nesim Alma on 19.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct IosMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
